b588036706f67c16f9bf02a4729c822f3882e574
ahmadheriyanto <ahmadheriyanto@yahoo.com>
2020-03-08 13:12:36 +0700

    update

---
+++ custom/so_identificationcode/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Sales Order Custom",
+    'version': '12.0.0.0.1',
+    'summary': """Identification Code""",
+    'description': """Add Identification Code into res.partner""",
+    'category': 'Sales',
+    'author': 'Ahmad Heriyanto',
+    'company': 'Nikisae',
+    'maintainer': 'Ahmad Heriyanto',
+    'website': "https://id.linkedin.com/in/ahmadheriyanto ",
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/res_partner.xml',
+    ],
+    'images': [],
+    'license': "AGPL-3",
+    'installable': True,
+    'application': False,
+}
