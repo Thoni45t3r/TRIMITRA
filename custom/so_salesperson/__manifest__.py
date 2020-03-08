b3b983315d852b15d7c483dbc8070043265c5bdb
ahmadheriyanto <ahmadheriyanto@yahoo.com>
2020-03-08 09:19:52 +0700

    update

---
+++ custom/so_salesperson/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Sales Order Custom",
+    'version': '12.0.0.0.1',
+    'summary': """Copy sales person field from tab into main header""",
+    'description': """Copy sales person field from tab into main header""",
+    'category': 'Sales',
+    'author': 'Ahmad Heriyanto',
+    'company': 'Nikisae',
+    'maintainer': 'Ahmad Heriyanto',
+    'website': "https://id.linkedin.com/in/ahmadheriyanto ",
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/salesperson.xml',
+    ],
+    'images': [],
+    'license': "AGPL-3",
+    'installable': True,
+    'application': False,
+}
