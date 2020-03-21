44293587b2cb7f7dee6d613c0d194f8ab4b79fa1
ahmadheriyanto <ahmadheriyanto@yahoo.com>
2020-03-21 11:37:05 +0700

    update

---
+++ custom/invoice_report_custom/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Invoice Report Custom",
+    'version': '12.0.0.0.1',
+    'summary': """Invoice Custom""",
+    'description': """Merubah Layout Invoice Doc Printout""",
+    'category': 'Accounting',
+    'author': 'Ahmad Heriyanto',
+    'company': 'Nikisae',
+    'maintainer': 'Ahmad Heriyanto',
+    'website': "https://id.linkedin.com/in/ahmadheriyanto ",
+    'depends': ['base', 'account'],
+    'data': [
+        "views/account_invoice_contact.xml",
+        "report/invoice_template_custom.xml",
+    ],
+    'images': [],
+    'license': "AGPL-3",
+    'installable': True,
+    'application': False,
+}
