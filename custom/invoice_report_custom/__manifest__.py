f6500eaabc41d0887599ab2d4e9a637ec1aa321f
ahmadheriyanto <ahmadheriyanto@yahoo.com>
2020-03-22 11:46:51 +0700

    update

--- custom/invoice_report_custom/__manifest__.py
+++ custom/invoice_report_custom/__manifest__.py
@@ -8,7 +8,7 @@
     'company': 'Nikisae',
     'maintainer': 'Ahmad Heriyanto',
     'website': "https://id.linkedin.com/in/ahmadheriyanto ",
-    'depends': ['base', 'account'],
+    'depends': ['base', 'account', 'sale'],
     'data': [
         "views/account_invoice_contact.xml",
         "report/invoice_template_custom.xml",
